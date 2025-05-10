f131b9e8dcdce46483cf516c7adb3d6ee2c99b3e
sanatannahak23 <sanatannahak23@gmail.com>
2025-05-10 17:11:05 +0530

    get user api added in controller

--- src/main/java/com/example/springboot_k8s_learn/controller/MyController.java
+++ src/main/java/com/example/springboot_k8s_learn/controller/MyController.java
@@ -3,11 +3,18 @@ package com.example.springboot_k8s_learn.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class MyController {
 
     @GetMapping("/greet")
     public String greet() {
         return "Welcome to kubernetes..";
+    }
+
+    @GetMapping("/get-users")
+    public List<String> getUserName() {
+        return List.of("suresh", "krunal", "rahul", "parakash", "Nihar");
     }
 }
